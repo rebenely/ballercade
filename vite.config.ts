cbbf20413c1cfaf064b13aaa069fee176832003b
rebenely <venrayyle@gmail.com>
2025-04-05 00:16:41 +0800

    remove dev code

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
-import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync('../192.168.50.55-key.pem'),
-      cert: fs.readFileSync('../192.168.50.55.pem'),
-    },
-  },
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
