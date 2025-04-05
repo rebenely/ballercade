08c9feddc63c6e740f2f0034c20ecf39f3a4b55c
rebenely <venrayyle@gmail.com>
2025-04-05 16:23:05 +0800

    only include certs if dev

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,23 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 import fs from 'fs';
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync('../192.168.50.55-key.pem'),
-      cert: fs.readFileSync('../192.168.50.55.pem'),
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+
+  return {
+    server: isDev
+      ? {
+          https: {
+            key: fs.readFileSync('../192.168.50.55-key.pem'),
+            cert: fs.readFileSync('../192.168.50.55.pem'),
+          },
+        }
+      : undefined,
+    plugins: [vue(), vueDevTools(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  plugins: [vue(), vueDevTools(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+  };
 });
