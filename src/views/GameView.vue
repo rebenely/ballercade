b51073da0f06d89a4e1bfc1de67cb999673077d9
rebenely <venrayyle@gmail.com>
2025-04-05 13:29:07 +0800

    remove scores from pinia store

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 import { useGameSound } from '@/composable/game-sound';
 import { useBallercade, type BallercadeStore } from '@/stores/ballercade';
-import { onMounted, onUnmounted } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 import { useAutoWakeLock } from '@/composable/wake-lock';
 import { ArcadeText, BallercadeButton } from '@/components';
 
 useAutoWakeLock();
+const gameScore = ref(0);
 
 const ballercade: BallercadeStore = useBallercade();
 onMounted(() => {
-  ballercade.freePlayScore = 0;
+  gameScore.value = 0;
   ballercade.setOnCharacteristicUpdate(() => {
-    ballercade.updateFreePlayScore();
+    gameScore.value++;
     playSound('score');
   });
 });
@@ -26,7 +27,7 @@ const { playSound } = useGameSound();
 <template>
   <main class="flex justify-center flex-col items-center gap-4">
     <h1 class="text-2xl">Score:</h1>
-    <ArcadeText class="text-9xl">{{ ballercade.freePlayScore }}</ArcadeText>
+    <ArcadeText class="text-9xl">{{ gameScore }}</ArcadeText>
     <BallercadeButton btn-type="a" to="/menu" class="fixed bottom-2 right-2">Exit</BallercadeButton>
   </main>
 </template>
